2bcdc2c5bc56c063b4e58c0cc0606949076a95ec
Titus <menkah10@gmail.com>
2023-02-05 17:21:29 +0000

    when new category is made it adds it to the nav

--- public/admin/nav.php
+++ public/admin/nav.php
@@ -1,11 +1,14 @@
+
 <nav>
 		<ul>
 			<li><a href="/">Home</a></li>
 			<li>Jobs
 				<ul>
-					<li><a href="it.php">IT</a></li>
-					<li><a href="hr.php">Human Resources</a></li>
-					<li><a href="sales.php">Sales</a></li>
+				<?php $categories = $pdo->query('SELECT * FROM category'); ?>
+				<?php foreach ($categories as $category): ?>
+               <li><a href="#"><?php echo $category['name']; ?></a></li>
+                  <?php endforeach; ?>
+				  
 
 				</ul>
 			</li>
@@ -15,3 +18,7 @@
 		
 
 	</nav>
+	
+	<!-- <li><a href="it.php">IT</a></li>
+					<li><a href="hr.php">Human Resources</a></li>
+					<li><a href="sales.php">Sales</a></li> -->
