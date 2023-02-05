9e60fe133d321ec501d6a23e13e13a038f6d94fd
Titus <menkah10@gmail.com>
2023-02-05 18:46:06 +0000

    function search

--- public/classes/DBTable.php
+++ public/classes/DBTable.php
@@ -2,5 +2,24 @@
 class DBTable{
     private $pdo;
     private $table;
-    
-}
+    private $pk;
+
+
+    public function __construct($pdo, $table, $pk){
+        $this->pdo = $pdo;
+        $this->table = $table;
+        $this->pk = $pk;
+    }
+
+    public function search($field, $value){
+        $stmt-> $this->pdo->prepare('SELECT * FROM' . $this->table. 'WHERE' . $field . '=:value');
+
+        $criteria =[
+            'value' => $value
+        ];
+        $stmt->execute($criteria);
+
+        return $stmt->fetchAll();
+    }
+
+  
